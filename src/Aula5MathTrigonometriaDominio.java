ed5caa9f5270411da97a153ddbe5cf2b1c93a3d0
Marieli Ficagna <marieli.ficagna@gmail.com>
2023-02-15 17:14:06 -0300

    first commit

---
+++ src/Aula5MathTrigonometriaDominio.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Mari (48) 99618-5728
+ */
+public class Aula5MathTrigonometriaDominio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double ponto1 = 30;
+        double ponto2 = 40;
+        System.out.println("A distancia entre o " 
+                + ponto1 + " e o " + ponto2 + " é "
+                + Math.hypot(ponto1, ponto2));
+        
+    }
+    
+}
